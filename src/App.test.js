725e96ede13932207d98e2bbe079a7d7effc1f0d
Pemba Sherpa <pemba_sherpa64@yahoo.com>
2021-11-25 18:22:20 -0500

    Home Page Completed

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,18 @@
-import {checkPrime} from './App';
+// import {checkValidity} from './App';
 
-test('check Prime', () => {
-  expect()  
-  
-});
+// describe('Checking the check prime function', () => {
+//   test('should handle empty case', () => {
+//     expect(checkValidity([])).toBeFalsy()
+//   });
+
+//   test('should handle comma separated', () => {
+//     expect(checkValidity("1,2,3,4,4,5,6,7")).toBeTruthy()
+//   })
+
+//   test('should not handle non comma separated values', () => {
+//     expect(checkValidity("12:2313:313:1")).toBeFalsy()
+//     expect(checkValidity("1213/123/123123/")).toBeFalsy()
+//     expect(checkValidity("1213123/123123/")).toBeFalsy()
+//   })
+
+// })
